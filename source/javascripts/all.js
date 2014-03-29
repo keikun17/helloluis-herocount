43ca636f77d34801af9d4f00b0bcc4e267526b1d
Buddy Magsipoc <keikun17@gmail.com>
2014-03-29 17:06:32 +0800

    Formify。

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -14,13 +14,18 @@ $(document).ready(function(){
             resp = data.responseText;
             grr = $.parseHTML(resp);
             $('#results').html('');
-            $(['druid','hunter','mage_13','rogue','warlock','warrior','priest','paladin','shaman']).map(function(){ return [[ ""+this, $(grr).find(".round1").find("img[src*='" + this + "']").length ]]; }).sort(function(a,b){ return b[1]-a[1]; }).each(function(){ 
+            $(['druid','hunter','mage_13','rogue','warlock','warrior','priest','paladin','shaman']).map(function(){ return [[ ""+this, $(grr).find(".round1").find("img[src*='" + this + "']").length ]]; }).sort(function(a,b){ return b[1]-a[1]; }).each(function(){
               $('#results').append(this[0] + ":" + this[1] + "<br>")
               console.log(this[0],this[1])
             });
           }
          );
   }
-  $('#do_it_faggot').on('click', function(){something($('#fagsource').val())} );
+
+  $('#do_it_faggot').on('submit', function(){
+    something($('#fagsource').val())
+    event.preventDefault();
+  } );
+
 })
 
