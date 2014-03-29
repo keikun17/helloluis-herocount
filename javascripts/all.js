66ccda89b4f41dff2543fc249e5255a165779659
Buddy Magsipoc <keikun17@gmail.com>
2014-03-29 16:11:34 +0800

    Site updated to 2292242

--- javascripts/all.js
+++ javascripts/all.js
@@ -9195,14 +9195,13 @@ $(document).ready(function(){
   var x = '';
 
   var something = function(url){
+    $('#results').text('loading...');
     $.get(url,
           function(data){
             x = data;
             resp = data.responseText;
             grr = $.parseHTML(resp);
-            console.log('-------');
-            console.log(grr);
-            console.log('-------');
+            $('#results').html('');
             $(['druid','hunter','mage_13','rogue','warlock','warrior','priest','paladin','shaman']).map(function(){ return [[ ""+this, $(grr).find(".round1").find("img[src*='" + this + "']").length ]]; }).sort(function(a,b){ return b[1]-a[1]; }).each(function(){ 
               $('#results').append(this[0] + ":" + this[1] + "<br>")
               console.log(this[0],this[1])
