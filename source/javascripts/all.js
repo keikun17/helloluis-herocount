0ee528a6a59272a108fe08ea96b6ec6850bec09b
Buddy Magsipoc <keikun17@gmail.com>
2014-03-30 23:26:13 +0800

    Should now count players under Round 1 of non tourneys。

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -13,10 +13,26 @@ $(document).ready(function(){
     $.get(url, function(data){
             resp = $.parseHTML(data.responseText);
             $('#results').html('');
-            $(['druid','hunter','mage_13','rogue','warlock','warrior','priest','paladin','shaman']).map(function(){ return [[ ""+this, $(resp).find(".round1").find("img[src*='" + this + "']").length ]]; }).sort(function(a,b){ return b[1]-a[1]; }).each(function(){
-              $('#results').append(this[0] + ":" + this[1] + "<br>")
-              console.log(this[0],this[1])
-            });
+
+            if( $(resp).find('.round1').length !=0 ){
+
+              // For tourney pages
+              $(['/druid','/hunter','/mage','/rogue','/warlock','/warrior','/priest','/paladin','/shaman']).map(function(){ return [[ ""+this, $(resp).find(".round1").find("img[src*='" + this + "']").length ]]; }).sort(function(a,b){ return b[1]-a[1]; }).each(function(){
+                $('#results').append(this[0].replace('/','') + ":" + this[1] + "<br>");
+                console.log(this[0],this[1]);
+              });
+
+            }else if( $(resp).find('#dragTable1').length != 0 ){
+
+              // For non tourney pages
+              console.log("in here");
+              window.shit = $(resp);
+              $(['/druid','/hunter','/mage','/rogue','/warlock','/warrior','/priest','/paladin','/shaman']).map(function(){ return [[ ""+this, $(resp).find("#dragTable1 td:first").find("img[src*='" + this + "']").length ]]; }).sort(function(a,b){ return b[1]-a[1]; }).each(function(){
+                $('#results').append(this[0].replace('/', '') + ":" + this[1] + "<br>");
+                console.log(this[0].replace('/',''),this[1]);
+              });
+
+            }
           }
          );
   }
